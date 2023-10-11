51d297da23622b0bf55b6f8ba24f6651b3df2d86
airfon <116084172+airfon@users.noreply.github.com>
2023-10-11 11:42:01 +0300

    Update ci-main-legacy.js

--- ci-main-legacy.js
+++ ci-main-legacy.js
@@ -6,15 +6,15 @@
             root.setAttribute("style", "height:100%");
             root.innerHTML = `
 				<style>	
-		   			#navigation-menu {width:70px;height:100%;overflow:hidden;border-right:1px solid #c9c9c9;transition:width .3s cubic-bezier(0,0.55,0.45,1) .5s;background-color:#ffffff}
+		   			#navigation-menu {width:70px;height:100%;overflow:hidden;border-right:1px solid #c9c9c9;transition:width .3s cubic-bezier(0,0.55,0.45,1) .5s;background-color:#0460A9}
 					#navigation-menu:hover {width:270px;transition:width .3s cubic-bezier(0,0.55,0.45,1)}
 					.menu-item-container,.menu-item-container-selected {position:relative;height:60px;width:100%;margin-top:10px;cursor:default}
 					.menu-item-container:first-child,.menu-item-container-selected:first-child {margin-top:60px}
-     					.menu-item-container-selected {background-color:#ebf2f8}
-					.menu-item-container:hover {background-color:#f5f9fc}
-					.menu-item-icon {position:absolute;display:block;top:14px;left:19px;cursor:default;fill:#9e9e9e}
-     					.menu-item-container-selected .menu-item-icon {fill:#0460A9}
-					.menu-item-text {position:absolute;top:21px;left:70px;width:200px;cursor:default;font-family:"72", Arial;color:#212121;font-weight:700;font-size:16px}
+     					.menu-item-container-selected {background-color:#2877B6}
+					.menu-item-container:hover {background-color:#2877B6}
+					.menu-item-icon {position:absolute;display:block;top:14px;left:19px;cursor:default;fill:#ffffff}
+     					.menu-item-container-selected .menu-item-icon {fill:#ffffff}
+					.menu-item-text {position:absolute;top:21px;left:70px;width:200px;cursor:default;font-family:"72", Arial;color:#ffffff;font-weight:700;font-size:16px}
 				</style>
 				<div id="navigation-menu"></div>`;
             this.appendChild(root);
